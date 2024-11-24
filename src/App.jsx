49eaef5b24d6b23d29723faa254f824308d883e2
rampradops28 <rampradop28@gmail.com>
2024-11-24 17:32:52 +0530

    Machine section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from "./pages/Footer";
 import About from "./pages/About";
 import Do from "./pages/Do";
 import Customer from "./components/home/Customer";
+import Machine from "./pages/Machine";
 
 function App() {
   return (
@@ -13,8 +14,11 @@ function App() {
       <Carousel />
       <About />
       <Do />
+      
       <Customer/>
+      <Machine />
       <Footer />
+      
     </>
   );
 }
